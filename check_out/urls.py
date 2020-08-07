acf015947c3d61c40c4160595e8a40895e7820ca
Paul Quinn <polocoinn@yahoo.ie>
2020-08-07 21:33:08 +0000

    Profiles 4 - add order history, update checkout views

--- check_out/urls.py
+++ check_out/urls.py
@@ -5,6 +5,6 @@ from .webhooks import webhook
 urlpatterns = [
     path('', views.check_out, name='check_out'),
     path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
-    path('cache_check_out_data/', views.cache_checkout_data, name='cache_check_out_data'),
+    path('cache_check_out_data/', views.cache_check_out_data, name='cache_check_out_data'),
     path('wh/', webhook, name='webhook'),
 ]
