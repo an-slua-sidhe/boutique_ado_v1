e6269818f694bdb34519b662444e238e3322a83e
Paul Quinn <polocoinn@yahoo.ie>
2020-08-07 13:48:33 +0000

    Add check_out.html and url.py

--- check_out/views.py
+++ check_out/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def check_out(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'check_out/check_out.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
